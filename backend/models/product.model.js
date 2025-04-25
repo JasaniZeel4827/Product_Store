fcb1270860fe959afa82b7a4198a301229dd00e6
Zeel <deltapixel8a@gmail.com>
2025-04-25 17:10:39 +0530

    Product_Store

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -13,4 +13,13 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+}, {
+    timestamps: true
 });
+
+
+const Product = mongoose.model('Product', productSchema);
+
+
+
+export default Product;
