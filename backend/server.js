7158b5cfc37467d69cc209e6b4b863448116c568
Zeel <deltapixel8a@gmail.com>
2025-04-25 14:54:34 +0530

    Product_Store

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,25 @@
 import express from 'express';
+import dotenv from "dotenv";
 
-const 
+dotenv.config();
+
+const app = express();
+
+
+app.get("/", (req,res) => {
+    res.send("server is ready in 1.. 2.. 3.. let's gooo")
+})
+
+
+console.log(process.env.MONGO_URI)
+
+
+
+app.listen(5000, () => {
+    console.log("zeel nu server start at http://localhost:5000 ")
+})
+
+
+
+
+// jVj3VTJpiUtwEWMd
