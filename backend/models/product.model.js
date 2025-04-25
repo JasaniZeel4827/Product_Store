e659859add5ae30689abc92b8ec7fec0ee356058
Zeel <deltapixel8a@gmail.com>
2025-04-25 16:01:40 +0530

    Product_Store

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,6 +1,16 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    type: String,
-    required: true
+    name:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    }
 });
